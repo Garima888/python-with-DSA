4949115a9e88f8dce4e1a94b1a6f1823e255ef55
Garima Patel <garimapatel951@gmail.com>
2025-07-02 17:01:20 +0530

    Add files via upload

---
+++ one dimensional array.py
@@ -0,0 +1,15 @@
+# One-Dimensional Array
+arr = [10, 20, 30, 40, 50]
+
+# Accessing elements
+print(arr[0])  # Output: 10
+
+# Inserting an element (at the end)
+arr.append(60)  # Now arr = [10, 20, 30, 40, 50, 60]
+
+# Deleting an element
+arr.remove(30)  # Now arr = [10, 20, 40, 50, 60]
+
+# Traversing the array
+for element in arr:
+    print(element)  # Output: 10, 20, 40, 50, 60
